ee33a9523c123c6989e0c40b9722c53795e25cc3
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-09-28 21:45:38 -0300

    feat: create function to hit the weather API

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './reset.css';
 import './styles.css';
+
+const getData = async function getDataFromAPI() {
+  try {
+    const response = await fetch(
+      'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK/today/next7days?key=7KYEEP4DDZDT24Y93QVRY86EC&include=days&elements=datetime,tempmax,tempmin,temp,humidity,precipprob,windspeed,description',
+      { mode: 'cors' },
+    );
+    const weatherData = await response.json();
+    console.log(weatherData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getData();
