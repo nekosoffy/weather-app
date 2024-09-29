2a68d98913551bc4cc4b2fb5968f4636cb17b0c9
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-09-29 20:14:06 -0300

    refactor: clean up code and separate concerns

---
+++ src/display.js
@@ -0,0 +1,111 @@
+const iconList = {};
+
+function importAll(moduleResolver) {
+  moduleResolver.keys().forEach(key => {
+    const iconName = key.replace('./', '').replace(/\.svg$/, '');
+    iconList[iconName] = moduleResolver(key);
+  });
+}
+
+importAll(require.context('./images', true, /\.svg$/));
+
+// Helper functions
+const select = target => document.querySelector(target);
+const selectId = target => document.getElementById(target);
+
+function create(element, parent, id, htmlClass, text) {
+  const el = document.createElement(element);
+
+  if (id !== undefined && id !== '') {
+    el.id = id;
+  }
+
+  if (htmlClass !== undefined && htmlClass !== '') {
+    el.classList.add(htmlClass);
+  }
+
+  if (text !== undefined && text !== '') {
+    el.textContent = text;
+  }
+
+  if (parent !== undefined && parent !== '') {
+    parent.appendChild(el);
+  }
+
+  return el;
+}
+
+function createImg(parent, source) {
+  const el = document.createElement('img');
+
+  el.src = source;
+
+  parent.appendChild(el);
+}
+//
+
+const main = document.querySelector('main');
+
+const clean = function resetSearchDisplayState() {
+  main.replaceChildren();
+  create('h2', main);
+  create('article', main);
+};
+
+const updateHeader = function updateHeaderSearchInfo(weatherData) {
+  const header = select('h2');
+  header.textContent = 'Results for: ';
+  create('span', header, '', '', `${weatherData.resolvedAddress}`);
+};
+
+const errorMessage = function displayErrorMessage(text) {
+  const header = select('h2');
+  header.textContent = text;
+};
+
+const updateSelected = function showSelectedDayInfo(i, weatherData) {
+  const selectedDay = create('article', '', 'selected-day');
+  const infoList = [
+    `Temperature: ${weatherData.days[i].temp}°`,
+    `Humidity: ${weatherData.days[i].humidity}%`,
+    `Rain: ${weatherData.days[i].precipprob}%`,
+    `Wind: ${weatherData.days[i].windspeed} km/h`,
+    `Description: ${weatherData.days[i].description}`,
+  ];
+
+  infoList.forEach((el, index) => {
+    create('p', selectedDay, '', 'property', `${infoList[index]}`);
+  });
+
+  createImg(selectedDay, iconList[weatherData.days[i].icon]);
+  main.appendChild(selectedDay);
+};
+
+const showWeek = function showWeekInfoCards(weatherData) {
+  const div = create('div', '', 'days-container');
+
+  weatherData.days.forEach(day => {
+    const article = create('article', '', '', 'day-wrapper');
+    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][
+      new Date(`${day.datetime}T00:00:00`).getDay()
+    ];
+    create('p', article, '', 'week-day', days);
+    createImg(article, iconList[day.icon]);
+    const textWrapper = create('div', article, '', 'text-wrapper');
+    create('p', textWrapper, '', 'temp-max', `${day.tempmax}°`);
+    create('p', textWrapper, '', 'temp-min', `${day.tempmin}°`);
+
+    div.appendChild(article);
+    main.appendChild(div);
+  });
+};
+
+export {
+  select,
+  selectId,
+  updateHeader,
+  errorMessage,
+  clean,
+  updateSelected,
+  showWeek,
+};
--- src/index.js
+++ src/index.js
@@ -1,59 +1,18 @@
 import './reset.css';
 import './styles.css';
-
-const iconList = {};
-
-function importAll(moduleResolver) {
-  moduleResolver.keys().forEach(key => {
-    const iconName = key.replace('./', '').replace(/\.svg$/, ''); // Remove extension
-    iconList[iconName] = moduleResolver(key);
-  });
-}
-
-importAll(require.context('./images', true, /\.svg$/));
-
-console.log(iconList);
-
-// Helper functions
-const select = target => document.querySelector(target);
-const selectId = target => document.getElementById(target);
-
-function create(element, parent, id, htmlClass, text) {
-  const el = document.createElement(element);
-
-  if (id !== undefined && id !== '') {
-    el.id = id;
-  }
-
-  if (htmlClass !== undefined && htmlClass !== '') {
-    el.classList.add(htmlClass);
-  }
-
-  if (text !== undefined && text !== '') {
-    el.textContent = text;
-  }
-
-  if (parent !== undefined && parent !== '') {
-    parent.appendChild(el);
-  }
-
-  return el;
-}
-
-function createImg(parent, source) {
-  const el = document.createElement('img');
-
-  el.src = source;
-
-  parent.appendChild(el);
-}
-//
+import {
+  select,
+  selectId,
+  updateHeader,
+  errorMessage,
+  clean,
+  updateSelected,
+  showWeek,
+} from './display';
 
 const input = selectId('location');
 const form = select('form');
 const inputNote = select('input + span');
-const header = select('h2');
-const main = document.querySelector('main');
 
 const getData = async function getDataFromAPI(location) {
   try {
@@ -64,11 +23,11 @@ const getData = async function getDataFromAPI(location) {
 
     if (!response.ok) {
       if (response.status === 400) {
-        console.error('Location not found. Please, try again.');
+        errorMessage('Location not found. Please, try again.');
       } else if (response.status === 500) {
-        console.error('There was an issue with the server. Try again later.');
+        errorMessage('There was an issue with the server. Try again later.');
       } else {
-        console.error(
+        errorMessage(
           `Unexpected error: ${response.status} - ${response.statusText}`,
         );
       }
@@ -76,59 +35,13 @@ const getData = async function getDataFromAPI(location) {
     }
 
     const weatherData = await response.json();
-    console.log(weatherData);
 
-    header.textContent = 'Results for: ';
-    const headerStyle = document.createElement('span');
-    headerStyle.textContent = `${weatherData.resolvedAddress}`;
-    header.append(headerStyle);
-
-    const selectedDay = create('article', '', 'selected-day');
-    const infoList = [
-      `Temperature: ${weatherData.days[0].temp}°`,
-      `Humidity: ${weatherData.days[0].humidity}%`,
-      `Rain: ${weatherData.days[0].precipprob}%`,
-      `Wind: ${weatherData.days[0].windspeed} km/h`,
-      `Description: ${weatherData.days[0].description}`,
-    ];
-
-    infoList.forEach((el, index) => {
-      create('p', selectedDay, '', 'property', `${infoList[index]}`);
-    });
-
-    createImg(selectedDay, iconList[weatherData.days[0].icon]);
-    main.appendChild(selectedDay);
-    const div = create('div', '', 'days-container');
-
-    weatherData.days.forEach((day, index) => {
-      const article = create('article', '', '', 'day-wrapper');
-      const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][
-        new Date(`${weatherData.days[index].datetime}T00:00:00`).getDay()
-      ];
-      const weekday = create('p', article, '', 'week-day', days);
-      const img = createImg(article, iconList[weatherData.days[index].icon]);
-      const textWrapper = create('div', article, '', 'text-wrapper');
-      const tempMin = create(
-        'p',
-        textWrapper,
-        '',
-        'temp-max',
-        `${weatherData.days[index].tempmax}°`,
-      );
-      const tempMax = create(
-        'p',
-        textWrapper,
-        '',
-        'temp-min',
-        `${weatherData.days[index].tempmin}°`,
-      );
-
-      div.appendChild(article);
-      main.appendChild(div);
-    });
+    clean();
+    updateHeader(weatherData);
+    updateSelected(0, weatherData);
+    showWeek(weatherData);
   } catch (error) {
-    console.error('An unexpected error occurred. Please try again later.');
-    console.log(error);
+    errorMessage('An unexpected error occurred. Please try again later.');
   }
 };
 
